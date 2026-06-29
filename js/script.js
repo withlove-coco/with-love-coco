7511dc780e1ba653e79995724246aacaf3d6118f
withlove-coco <takesakusei@gmail.com>
2026-06-29 22:24:47 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ $(function(){
  $(document).ready(function(){
       $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 5000,
         speed: 3000,
         fade: true,
         arrows: false,
